1bc4893fd2dd71dca7b53f1881f7b61d8ae48ba2
Takudzwa Frank Mukarakate <mukarakatetf@gmail.com>
2021-08-13 17:52:46 +1000

    Complete hello world console via literal, variable and constant

---
+++ HelloWorld/hello-world-console.js
@@ -0,0 +1,17 @@
+/**
+ * @Author Takudzwa Frank Mukarakate
+ *
+ * Traditional introductory "Hello World!" console script
+ * Outputs Hello World with various methods.
+ */
+
+// String literal
+console.log("Hello World!");
+
+// String variable
+var helloText = "Hello World!";
+console.log("Variable: " + helloText);
+
+// String constant
+const HELLO_WORLD = "Hello World!";
+console.log("Constant: " + HELLO_WORLD);
